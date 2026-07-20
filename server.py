326e5afec3d14068ead67ba941961cc21bbde504
Zeinoul <zeinouldiop0@gmail.com>
2026-07-20 14:39:09 +0000

    version complète bot sunu avec firebase

---
+++ server.py
@@ -0,0 +1,23 @@
+    import os
+    from flask import Flask, request
+    import requests
+
+    app = Flask(__name__)
+
+    VERIFY_TOKEN = "sunu2026"
+    PAGE_ACCESS_TOKEN = os.environ.get("PAGE_ACCESS_TOKEN")
+
+    @app.route('/webhook', methods=['GET'])
+    def verify():
+        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            return request.args.get("hub.challenge")
+        return "Erreur de vérification", 403
+
+    @app.route('/webhook', methods=['POST'])
+    def webhook():
+        data = request.get_json()
+        print(data) # pour voir dans les logs Render
+        return "ok", 200
+
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
